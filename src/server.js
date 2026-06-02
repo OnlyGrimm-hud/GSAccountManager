0b83641d88c442890dfc00d92273d5984d8b06e7
OnlyGrimm-hud <patrick.daugherty57@gmail.com>
2026-06-01 19:10:45 -0700

    Prepare server for Render deployment

--- src/server.js
+++ src/server.js
@@ -21,9 +21,11 @@ const sensitiveFields = [
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/../views`);
 app.disable('x-powered-by');
+if (config.nodeEnv === 'production') app.set('trust proxy', 1);
 app.use(express.static(`${__dirname}/../public`));
 app.use(express.urlencoded({ extended: false, limit: '1mb' }));
 app.use(express.json({ limit: '1mb' }));
+app.get('/healthz', (req, res) => res.status(200).send('OK'));
 app.use(session({
   store: new PgSession({ pool: db.pool, createTableIfMissing: true }),
   name: 'gsam.sid',
@@ -48,8 +50,6 @@ app.use((req, res, next) => {
   next();
 });
 app.use(csrf);
-
-app.get('/healthz', (req, res) => res.status(200).send('OK'));
 
 app.get('/login', (req, res) => res.render('login', { title: 'Login', error: null }));
 app.post('/login', (req, res) => {
