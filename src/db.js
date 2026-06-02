c0cd8991022ce63564a307b31bb089ac22bcd8b6
OnlyGrimm-hud <patrick.daugherty57@gmail.com>
2026-06-01 18:55:24 -0700

    Add server modules

---
+++ src/db.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const { Pool } = require('pg');
+const config = require('./config');
+
+const pool = new Pool({
+  connectionString: config.databaseUrl,
+  ssl: config.nodeEnv === 'production' ? { rejectUnauthorized: false } : false
+});
+
+async function migrate() {
+  if (!config.autoMigrate) return;
+  const schema = fs.readFileSync(path.join(__dirname, '..', 'db', 'init.sql'), 'utf8');
+  await pool.query(schema);
+}
+
+async function query(text, params) {
+  return pool.query(text, params);
+}
+
+async function close() {
+  await pool.end();
+}
+
+module.exports = { pool, query, migrate, close };
